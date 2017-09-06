c33c4d3c25c5e326e41b9804e881b232bc316e49
Francisco Romano <blastsilver@gmail.com>
2017-09-06 13:06:38 +1000

    First commit

---
+++ Bright Tunel/Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+	public GameObject prefabBase = null;
+
+	public List<GameObject> Generate(Vector3 start, Vector3 offset, int amount)
+	{
+		// required variables
+		Vector3 position = start;
+		List<GameObject> list = new List<GameObject>();
+		// iterate through spawn amount
+		for (int i = 0; i < amount; i++)
+		{
+			// instantiate object & append to list
+			list.Add(GameObject.Instantiate(prefabBase, position, new Quaternion(), transform));
+			// update position
+			position += offset;
+		}
+		// return object list
+		return list;
+	}
+}
---
+++ Bright Tunel/Assets/Scripts/WorldManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldManager : MonoBehaviour
+{
+	public int amount = 5;
+	public Vector3 direction = new Vector3(0, 0, 0);
+	public Vector3 generationStart = new Vector3(0, 0, 0);
+	public Vector3 generationOffset = new Vector3(0,-1, 0);
+
+	private WorldGenerator m_WorldGenerator;
+	private List<GameObject> m_WorldObjects = new List<GameObject>();
+
+
+
+	void Start()
+	{
+		// fetch data
+		m_WorldGenerator = GetComponent<WorldGenerator> ();
+		// generate new world
+		List<GameObject> list = m_WorldGenerator.Generate(generationStart, generationOffset, amount);
+		// update generated objects
+		AppendObjects(list);
+	}
+
+	void Update ()
+	{
+		Vector3 dir = direction * Time.deltaTime;
+
+		foreach (GameObject obj in m_WorldObjects)
+		{
+			if (obj.transform.position.y < 2)
+			{
+				obj.transform.Translate(dir);
+			}
+			else
+			{
+				Destroy(obj);
+				m_WorldObjects.Remove(obj);
+			}
+		}
+	}
+
+	public void AppendObjects(GameObject obj) { m_WorldObjects.Add (obj); }
+	public void AppendObjects(List<GameObject> list) { foreach (GameObject obj in list) AppendObjects (obj); }
+}
